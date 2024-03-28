909d2030264c08fce95e37850b0c4b41af55454a
Deepak <cdeepakdpm@outlook.com>
2024-03-28 20:22:16 +0530

    LoginPageLoaded

--- imsapp/src/app/app-routing.module.ts
+++ imsapp/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' }, // Default route redirects to login
+  { path: 'login', component: LoginComponent },
+  // Other routes here
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
