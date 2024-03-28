f9278024bf5a7e1cefbff278582d6ff2ea8637ef
Deepak <cdeepakdpm@outlook.com>
2024-03-28 20:51:30 +0530

    LoginValidationCompleted

--- imsapp/src/app/components/login/login.component.ts
+++ imsapp/src/app/components/login/login.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  userDetails: any ={
+    userName: 'Deepak',
+    passWord: '12345'
+  }
+loginObj: any ={
+  userName: '',
+  passWord: ''
+}
+onLogin()
+{
+  const isValid = (this.userDetails.userName == this.loginObj.userName) && (this.userDetails.passWord == this.loginObj.passWord)
+  if(isValid == true)
+  {
+  alert("Login Successfull");
+  }
+  else
+  {
+    alert("Invalid User" + isValid);
+  }
+}
+ngOnInit(): void {
+  
+}
 
 }
